2d142541bfaadfd69297ea86324f1ae7f77602b6
donKurilio <adskiyyo@mail.ru>
2018-01-13 22:50:41 +0300

    first implementation of training;

--- main.py
+++ main.py
@@ -12,6 +12,9 @@ bot = telebot.TeleBot(config.token)
 # слово-перевод, хрень
 temp_words = {}
 
+# текущие тренировки пользователей
+trainings = {}
+
 
 @bot.message_handler(commands=["start"])
 def start_messaging(message):
@@ -60,6 +63,19 @@ def get_all_words(message):
     bot.send_message(message.chat.id, reply)
 
 
+# перевод пользователя в состояние добавления карточки
+@bot.message_handler(commands=["training"])
+def training_cmd_handler(message):
+    user_id = message.from_user.id
+    words = get_words_for_training(user_id)
+    if len(words) > 0:
+        set_user_state(user_id, "training", db)
+        trainings[user_id] = words
+        bot.send_message(message.chat.id, 'Введите перевод слова: ' + trainings[user_id][0][0])
+    else:
+        bot.send_message(message.chat.id, 'Список слов пуст. Сначала добавьте слова')
+
+
 # обработка сообщений от пользователей, находящихся в состоянии добавления англ. слова
 @bot.message_handler(func=lambda message: get_user_state(message.from_user.id, db) == "adding_word")
 def adding_word(message):
@@ -86,6 +102,29 @@ def adding_translation(message):
     bot.send_message(message.chat.id, 'Запись добавлена: ' + word + ':' + message.text, reply_markup=markup)
 
 
+# обработка сообщений от пользователей, находящихся в состоянии тренировки
+@bot.message_handler(func=lambda message: get_user_state(message.from_user.id, db) == "training")
+def user_training_handler(message):
+    user_id = message.from_user.id
+    card = trainings[user_id].pop(0)
+
+    word, translation = card[0], card[1]
+    user_answer = message.text.lower().strip()
+
+    success = user_answer == translation
+    change_score(user_id, word, success)
+
+    reply = 'Верно!' if success else 'Неверно! Правильный ответ: ' + translation
+
+    if len(trainings[user_id]) == 0:
+        set_user_state(user_id, "start", db)
+        reply += ' Тренировка окончена'
+    else:
+        reply += '\nВведите перевод слова: ' + trainings[user_id][0][0]
+
+    bot.send_message(message.chat.id, reply)
+
+
 # добавление пользователья в бд
 def add_user(id):
     db_state = db.prepare("INSERT INTO user_states(user_id, state) VALUES ($1, $2)")
@@ -100,13 +139,23 @@ def add_word(user_id, word, translation):
 
 # функция выбора слов для тренировки
 def get_words_for_training(user_id):
-    db_words = db.prepare('SELECT *, CASE WHEN sum_en = 0 THEN 0 ELSE score_en/sum_en END "koef" \
-                           FROM word_translations \
-                           WHERE user_id = $1 \
-                           ORDER BY "koef" DESC \
-                           LIMIT 5')
+    db_words = db.prepare('\
+        SELECT word_en "word", word_ru "translation", \
+        CASE WHEN sum_en = 0 THEN 0 \
+        	 ELSE score_en/sum_en \
+             END "koef"\
+        FROM word_translations\
+        WHERE user_id = $1\
+        ORDER BY "koef" DESC\
+        LIMIT 5')
     return db_words(user_id)
 
+
+def change_score(user_id, word, success = True):
+    query_text = 'UPDATE word_translations SET score_en = score_en ' + ('+ 1' if success else '') + ', sum_en = sum_en + 1 ' \
+               + 'WHERE user_id = $1 AND word_en = $2'
+    prepared_query = db.prepare(query_text)
+    prepared_query(user_id, word)
 
 if __name__ == '__main__':
     bot.polling(none_stop=True)

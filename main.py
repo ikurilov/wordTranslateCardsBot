2e84fd31eb95c01a1d944094c468383cf25f3921
vadimbandel <vadim@bandel.ru>
2018-01-21 21:42:51 +0300

    the end(but it is not exactly)

--- main.py
+++ main.py
@@ -18,8 +18,7 @@ def start_messaging(message):
     set_user_state(user_id, 'start', db)
     bot.send_message(message.chat.id,
                      "Здравствуйте! Наш бот может работать для вас в качестве вашего личного словаря, а также поможет запомнить трудно дающиеся вам слова.")
-    bot.send_message(message.chat.id, 'Введите "/" и выбирите одну из предложенных команд: ')
-
+    end_any_operation(message)
 
 
 @bot.callback_query_handler(func=lambda call: True)
@@ -31,12 +30,19 @@ def callback_inline(call):
             bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text="Пыщь")
 
 
+# ловим конец операций
+@bot.message_handler(commands=["end"])
+def end_any_operation(message):
+    set_user_state(message.from_user.id, 'start', db)
+    help_show_main_message(message)
+
+
 # перевод пользователя в состояние добавления карточки
 @bot.message_handler(commands=["add"])
 def add_word_handler(message):
     user_id = message.from_user.id
     set_user_state(user_id, "adding_word", db)
-    bot.send_message(message.chat.id, 'Вводите карточки в формате word - перевод. /end - закончить: ')
+    bot.send_message(message.chat.id, 'Вводите карточки в формате слово - перевод. /end - закончить: ')
 
 
 # все слова, для тестов
@@ -47,39 +53,50 @@ def get_all_words(message):
     reply = ''
     for i in words(user_id):
         reply += i[0] + ' - ' + i[1] + '\n'
-    bot.send_message(message.chat.id, reply)
-    set_user_state(user_id, 'start', db)
+    if reply == '':
+        bot.send_message(message.chat.id, 'Список ваших карточек пуст.')
+    else:
+        bot.send_message(message.chat.id, reply)
+
+    end_any_operation(message)
 
 
 # ловим пользователя находящегося в состоянии удаления своей карточки
-@bot.message_handler(func=lambda message: get_user_state(message.from_user.id, db) == 'word_deleting')
-def delete_word(message):
+@bot.message_handler(func=lambda message: get_user_state(message.from_user.id, db) == 'card_deleting')
+def delete_card(message):
     user_id = message.from_user.id
-    word_count = db.prepare('select count(*) from word_translations where user_id = $1')
-    word_count(user_id)
-    str_count_before = re.findall(r'\d+', str(word_count))
-    print(str_count_before)
-    word_deleting = db.prepare('delete from word_translations where user_id = $1 and (word_en = $2 or word_ru = $2)')
-    word_deleting(message.from_user.id, message.text)
-
-    word_count(user_id)
-    str_count_after = re.findall(r'\d+', str(word_count))
-    print(str_count_after)
-
-    set_user_state(user_id, 'start', db)
+    card_deleting = db.prepare('delete from word_translations where user_id = $1 and (word_en = $2 or word_ru = $2)')
+    result_deleting = card_deleting(message.from_user.id, message.text)
+    if result_deleting[1] == 0:
+        bot.send_message(message.chat.id, 'Карточка не найдена! Повторите ввод карточки или напишите /end для выхода.')
+    else:
+        bot.send_message(message.chat.id, 'Карточка успешно удалена.')
 
 
 # помещаем пользователя в состояние удаления своей карточки
 @bot.message_handler(commands=["delete_card"])
 def delete_word_state(message):
-    set_user_state(message.from_user.id, 'word_deleting', db)
+    set_user_state(message.from_user.id, 'card_deleting', db)
     bot.send_message(message.chat.id, 'Введите либо слово либо перевод, которые хотите удалить: ')
 
 
-# ловим конец операций
-@bot.message_handler(commands=["end"])
-def end_adding_words(message):
-    set_user_state(message.from_user.id, 'start', db)
+# ловим пользователя находящегося в состоянии удаления всех своих карточек
+@bot.message_handler(func=lambda message: get_user_state(message.from_user.id, db) == 'deleting_all_cards')
+def delete_all_cards(message):
+    if message.text == 'yes':
+        all_cards_deleting = db.prepare('delete from word_translations where user_id = $1')
+        all_cards_deleting(message.from_user.id)
+        bot.send_message(message.chat.id, 'Все ваши карточки успешно удалены!')
+    else:
+        bot.send_message(message.chat.id, 'Вы отменили удаление.')
+    end_any_operation(message)
+
+
+# помещаем пользователя в состояние удаления всех своих карточек
+@bot.message_handler(commands=["delete_all_cards"])
+def delete_all_cards_state(message):
+    set_user_state(message.from_user.id, 'deleting_all_cards', db)
+    bot.send_message(message.chat.id, 'Вы действительно хотите удалить все ваши карточки? (yes).')
 
 
 # перевод пользователя в состояние добавления карточки
@@ -157,17 +174,30 @@ def get_words_for_training(user_id):
     return db_words(user_id)
 
 
-@bot.message_handler(func=lambda message: get_user_state(message.from_user.id, db) == "start")
-def help_show_main_message(message):
-    bot.send_message(message.chat.id, 'Введите "/" и выбирите одну из предложенных команд: ')
-
-
 def change_score(user_id, word, success=True):
     query_text = 'UPDATE word_translations SET score_en = score_en ' + (
         '+ 1' if success else '') + ', sum_en = sum_en + 1 ' \
                  + 'WHERE user_id = $1 AND word_en = $2'
     prepared_query = db.prepare(query_text)
     prepared_query(user_id, word)
+
+
+@bot.message_handler(commands=["help"])
+def show_commands(message):
+    reply = ''
+    reply += '/add - Добавить карточку слово - перевод\n'
+    reply += '/training - Начать тренировку\n'
+    reply += '/show_all_cards - Список всех ваших карточек\n'
+    reply += '/delete_card - Удалить карточку\n'
+    reply += '/delete_all_cards - Удалить все карточки\n'
+    reply += '/help - Cписок доступных команд\n'
+    bot.send_message(message.chat.id, reply)
+    set_user_state(message.from_user.id, 'start', db)
+
+
+@bot.message_handler(func=lambda message: get_user_state(message.from_user.id, db) == "start")
+def help_show_main_message(message):
+    bot.send_message(message.chat.id, 'Для списка доступных команд отправьте "/help" или просто введите "/".')
 
 
 if __name__ == '__main__':
